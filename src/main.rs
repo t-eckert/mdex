9a599acfe715576745e500f541c5d8d3060c88ba
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-03-01 20:46:47 -0500

    Display markdown!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use comrak::{markdown_to_html, ComrakOptions};
+use rocket_dyn_templates::{context, Template};
+use std::fs;
+use std::path::{Path, PathBuf};
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/<path..>")]
+fn index(path: PathBuf) -> Template {
+    let path_to_file = Path::new("../../Notebook/").join(path);
+
+    let file = fs::read(path_to_file).ok();
+
+    Template::render(
+        "layout",
+        context! {
+            title: "Hello",
+            body: markdown_to_html(&String::from_utf8_lossy(&file.unwrap()).to_string(), &ComrakOptions::default()),
+        },
+    )
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(Template::custom(|engines| {
+            engines.tera.autoescape_on(vec![])
+        }))
+        .mount("/", routes![index])
 }
